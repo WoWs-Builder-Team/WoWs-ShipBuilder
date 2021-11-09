3d242420fe8fc147c8cc4dbd43225cfea31b29ea
yesod30 <samu.9301295@live.it>
2021-11-09 10:01:13 +0100

    IMplemented double tap for build loading

--- WoWsShipBuilder.UI/Views/StartingMenuWindow.axaml.cs
+++ WoWsShipBuilder.UI/Views/StartingMenuWindow.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using WoWsShipBuilder.UI.ViewModels;
 
 namespace WoWsShipBuilder.UI.Views
 {
@@ -17,6 +18,12 @@ namespace WoWsShipBuilder.UI.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void LoadBuild(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var dc = (StartMenuViewModel)DataContext!;
+            dc.LoadBuild(null!);
         }
     }
 }
