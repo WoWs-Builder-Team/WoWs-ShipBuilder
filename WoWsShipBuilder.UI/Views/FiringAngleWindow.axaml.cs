5263572caa2ab2e47461446c96fe24ba7ba50fdc
yesod30 <samu.9301295@live.it>
2021-11-11 19:03:57 +0100

    fixed discord link

--- WoWsShipBuilder.UI/Views/FiringAngleWindow.axaml.cs
+++ WoWsShipBuilder.UI/Views/FiringAngleWindow.axaml.cs
@@ -35,7 +35,7 @@ namespace WoWsShipBuilder.UI.Views
 
         private void OpenDiscord(object? sender, Avalonia.Input.PointerReleasedEventArgs e)
         {
-            string url = "https://discord.gg/GpeUF6Xp9X";
+            string url = "https://discord.gg/C8EaepZJDY";
 
             Process.Start(new ProcessStartInfo
             {
--- WoWsShipBuilder.UI/Views/SettingsWindow.axaml.cs
+++ WoWsShipBuilder.UI/Views/SettingsWindow.axaml.cs
@@ -22,7 +22,7 @@ namespace WoWsShipBuilder.UI.Views
 
         private void OpenDiscord(object? sender, Avalonia.Input.PointerReleasedEventArgs e)
         {
-            string url = "https://discord.gg/GpeUF6Xp9X";
+            string url = "https://discord.gg/C8EaepZJDY";
 
             Process.Start(new ProcessStartInfo
             {
