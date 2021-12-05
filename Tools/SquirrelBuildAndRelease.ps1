96b637b6aae1a3a1d44a91ffac080b09ad170aa7
floribe2000 <43866194+floribe2000@users.noreply.github.com>
2021-12-06 00:43:17 +0100

    Change release workflow to use application version from file.

---
+++ Tools/SquirrelBuildAndRelease.ps1
@@ -0,0 +1,14 @@
+﻿param(
+    [string]$buildConfig="Release"
+)
+
+Write-Output "Building application"
+dotnet build -c $buildConfig
+Write-Output "Publishing build"
+dotnet publish WoWsShipBuilder.UI -c $buildConfig -p:PublishProfile=PublishWindows
+Copy-Item -Path WoWsShipBuilder.UI\bin\$buildConfig\net5.0\runtimes\win7-x64\native\av_libGLESv2.dll -Destination WoWsShipBuilder.UI\bin\$buildConfig\net5.0\publish\
+Write-Output "Packing published build into nuget package"
+dotnet pack WoWsShipBuilder.UI -c $buildConfig --no-build
+Write-Output "Creating Squirrel.Windows release"
+Tools\Squirrel.com --releasify (Get-Item -Path WoWsShipBuilder.UI\bin\$buildConfig\WoWsShipBuilder.*.nupkg) --selfContained --releaseDir=".\releases" --setupIcon="WoWsShipBuilder.UI\Assets\ShipBuilderIcon_bg.ico" --icon="WoWsShipBuilder.UI\Assets\ShipBuilderIcon_bg.ico" --no-delta --no-msi --loadingGif="LoadingIcon.gif"
+Write-Output "Local release test completed"
