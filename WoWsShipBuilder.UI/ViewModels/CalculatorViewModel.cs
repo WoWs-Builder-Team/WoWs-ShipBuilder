caf512d694eba2255eb4b1df8cde0ca28bcfc810
ephrs <Jr6ABmueGf6nYkx>
2022-02-14 20:09:00 +0100

    GetCamos in VM

--- WoWsShipBuilder.UI/ViewModels/CalculatorViewModel.cs
+++ WoWsShipBuilder.UI/ViewModels/CalculatorViewModel.cs
@@ -4,13 +4,26 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DynamicData;
+using WoWsShipBuilder.Core.DataProvider;
 using WoWsShipBuilder.Core.DataUI;
+using WoWsShipBuilder.DataStructures;
 
 namespace WoWsShipBuilder.UI.ViewModels
 {
     public class CalculatorViewModel : ViewModelBase
     {
-        private ObservableCollection<ExteriorUI> exterior;
+        public ObservableCollection<ExteriorUI> Exterior { get; set; }
+
+        public CalculatorViewModel()
+        {
+            Exterior = new((IEnumerable<ExteriorUI>)AppDataHelper.Instance.GetCommonExterior());
+        }
+
+        private void GetShipPermoFlages(Ship ship)
+        {
+            Exterior.AddRange((IEnumerable<ExteriorUI>)AppDataHelper.Instance.GetShipExterior(ship.Permaflages));
+        }
     }
 }
 
