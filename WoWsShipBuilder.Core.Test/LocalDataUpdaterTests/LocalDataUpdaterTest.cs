57d241de87136b39d0578ad347be9cc3ef28e958
floribe2000 <git@floribe2000.de>
2022-02-20 20:59:12 +0100

    Fix unittests

--- WoWsShipBuilder.Core.Test/LocalDataUpdaterTests/LocalDataUpdaterTest.cs
+++ WoWsShipBuilder.Core.Test/LocalDataUpdaterTests/LocalDataUpdaterTest.cs
@@ -4,6 +4,7 @@ using Moq;
 using NUnit.Framework;
 using WoWsShipBuilder.Core.DataProvider;
 using WoWsShipBuilder.Core.HttpClients;
+using WoWsShipBuilder.Core.Services;
 using WoWsShipBuilder.DataStructures;
 
 namespace WoWsShipBuilder.Core.Test.LocalDataUpdaterTests
@@ -15,14 +16,15 @@ namespace WoWsShipBuilder.Core.Test.LocalDataUpdaterTests
 
         private Mock<IAwsClient> awsClientMock = default!;
 
-        private Mock<DesktopAppDataService> appDataHelper = default!;
+        private Mock<IAppDataService> appDataHelper = default!;
 
         [SetUp]
         public void Setup()
         {
             mockFileSystem = new MockFileSystem();
             awsClientMock = new Mock<IAwsClient>();
-            appDataHelper = new Mock<DesktopAppDataService>(MockBehavior.Loose, mockFileSystem);
+            appDataHelper = new Mock<IAppDataService>();
+            appDataHelper.Setup(x => x.GetDataPath(ServerType.Live)).Returns(@"C:\AppData\live");
         }
 
         private VersionInfo CreateTestVersionInfo(int versionCode, GameVersion? gameVersion = null)
