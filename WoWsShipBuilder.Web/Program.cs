fed30e015614980cb192e2658b3cc9891219a7d8
floribe2000 <git@floribe2000.de>
2022-03-08 17:03:39 +0100

    Configure Blazorise and update demo application to work with Blazorise

--- WoWsShipBuilder.Web/Program.cs
+++ WoWsShipBuilder.Web/Program.cs
@@ -1,11 +1,25 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ReactiveUI;
+using Splat;
+using Splat.Microsoft.Extensions.DependencyInjection;
 using WoWsShipBuilder.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.UseMicrosoftDependencyResolver();
+var resolver = Locator.CurrentMutable;
+resolver.InitializeSplat();
+resolver.InitializeReactiveUI();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazorise(options => { options.Immediate = true; })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
