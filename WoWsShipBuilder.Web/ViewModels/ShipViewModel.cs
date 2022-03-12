2779b5e6b2fece5ee96e73cee25fe6befacff0c3
floribe2000 <git@floribe2000.de>
2022-03-12 18:48:36 +0100

    Create some test components for web app

---
+++ WoWsShipBuilder.Web/ViewModels/ShipViewModel.cs
@@ -0,0 +1,18 @@
+﻿using WoWsShipBuilder.Core.Data;
+using WoWsShipBuilder.Core.Services;
+using WoWsShipBuilder.ViewModels.ShipVm;
+
+namespace WoWsShipBuilder.Web.ViewModels;
+
+public class ShipViewModel : MainWindowViewModelBase
+{
+    public ShipViewModel(INavigationService navigationService, IAppDataService appDataService, MainViewModelParams viewModelParams)
+        : base(navigationService, appDataService, viewModelParams)
+    {
+    }
+
+    public override void OpenSaveBuild()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ WoWsShipBuilder.Web/ViewModels/TestVm.cs
@@ -0,0 +1,20 @@
+﻿using ReactiveUI;
+using WoWsShipBuilder.ViewModels.Base;
+
+namespace WoWsShipBuilder.Web.ViewModels;
+
+public class TestVm : ViewModelBase
+{
+    public TestVm(string initialSelect)
+    {
+        selectedItem = initialSelect;
+    }
+
+    private string selectedItem;
+
+    public string SelectedItem
+    {
+        get => selectedItem;
+        set => this.RaiseAndSetIfChanged(ref selectedItem, value);
+    }
+}
