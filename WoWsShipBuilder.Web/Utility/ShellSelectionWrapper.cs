339380c1e0165290708480c8c5d2e70cf68f02ba
iTTou <perotti.denis@gmail.com>
2022-03-29 23:59:30 +0200

    Several code improvements to plots

--- WoWsShipBuilder.Web/Utility/ShellSelectionWrapper.cs
+++ WoWsShipBuilder.Web/Utility/ShellSelectionWrapper.cs
@@ -4,20 +4,29 @@ namespace WoWsShipBuilder.Web.Utility;
 
 public class ShellSelectionWrapper
 {
-    public string ShipIndex = string.Empty;
-    public Dictionary<ArtilleryData, bool> ArtilleryDictionary = new();
-
     public ShellSelectionWrapper(string shipIndex, IEnumerable<ArtilleryData> artilleryData)
     {
         ShipIndex = shipIndex;
-        ArtilleryDictionary = artilleryData.ToDictionary(x => x, _ => false);
+        ArtilleryDataDictionary = artilleryData.ToDictionary(x => x, _ => false);
     }
+
+    public string ShipIndex { get; }
+
+    public Dictionary<ArtilleryData, bool> ArtilleryDataDictionary { get; }
 }
 
 public sealed record ArtilleryData(ArtilleryShell Shell, Dispersion DispersionValues, string ShellIndex, double MaxRange)
 {
-    public ArtilleryShell Shell { get; } = Shell;
+    public ArtilleryData(string shellIndex)
+        : this(new(), new(), shellIndex, default)
+    {
+    }
+
+    public ArtilleryShell Shell { get; set; } = Shell;
+
     public string ShellIndex { get; } = ShellIndex;
-    public double MaxRange { get; } = MaxRange;
-    public Dispersion DispersionValues { get; } = DispersionValues;
+
+    public double MaxRange { get; set;  } = MaxRange;
+
+    public Dispersion DispersionValues { get; set; } = DispersionValues;
 }
