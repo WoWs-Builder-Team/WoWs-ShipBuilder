567f1f9f976814daa0b9a103581dd8e183173f52
iTTou <perotti.denis@gmail.com>
2022-03-30 21:54:34 +0200

    code fixes and improvements for charts data

--- WoWsShipBuilder.Web/Utility/ShellSelectionWrapper.cs
+++ WoWsShipBuilder.Web/Utility/ShellSelectionWrapper.cs
@@ -4,29 +4,29 @@ namespace WoWsShipBuilder.Web.Utility;
 
 public class ShellSelectionWrapper
 {
-    public ShellSelectionWrapper(string shipIndex, IEnumerable<ArtilleryData> artilleryData)
+    public ShellSelectionWrapper(string shipIndex, Dictionary<string, ArtilleryData> artilleryData)
     {
         ShipIndex = shipIndex;
-        ArtilleryDataDictionary = artilleryData.ToDictionary(x => x, _ => false);
+        ArtilleryDataDictionary = artilleryData;
     }
 
     public string ShipIndex { get; }
 
-    public Dictionary<ArtilleryData, bool> ArtilleryDataDictionary { get; }
+    public double MaxRange { get; set; } = -1;
+
+    public Dictionary<string, ArtilleryData> ArtilleryDataDictionary { get; }
 }
 
-public sealed record ArtilleryData(ArtilleryShell Shell, Dispersion DispersionValues, string ShellIndex, double MaxRange)
+public sealed record ArtilleryData(ArtilleryShell Shell, Dispersion DispersionValues, bool ShellSelected)
 {
-    public ArtilleryData(string shellIndex)
-        : this(new(), new(), shellIndex, default)
+    public ArtilleryData()
+        : this(new(), new(), false)
     {
     }
 
     public ArtilleryShell Shell { get; set; } = Shell;
 
-    public string ShellIndex { get; } = ShellIndex;
-
-    public double MaxRange { get; set;  } = MaxRange;
-
     public Dispersion DispersionValues { get; set; } = DispersionValues;
+
+    public bool ShellSelected { get; set; } = ShellSelected;
 }
