77a36e1c6367b95e759866a102225944f6e389a5
yesod30 <samu.9301295@live.it>
2022-03-31 19:12:58 +0200

    Webworkers test implementation

---
+++ WoWsShipBuilder.Web/Services/ServiceCollectionHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace WoWsShipBuilder.Web.Services;
+
+public static class ServiceCollectionHelper
+{
+    public delegate void Configure(IServiceCollection services);
+
+    public static IServiceProvider BuildServiceProviderFromMethod(Configure configureMethod)
+    {
+        var serviceCollection = new ServiceCollection();
+        configureMethod(serviceCollection);
+        return serviceCollection.BuildServiceProvider();
+    }
+}
