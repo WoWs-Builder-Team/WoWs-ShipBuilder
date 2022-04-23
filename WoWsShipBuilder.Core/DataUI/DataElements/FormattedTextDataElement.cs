68f56b10fb3e3a1ca924084e2f8be3a0ad8fddf6
yesod30 <samu.9301295@live.it>
2022-04-23 12:11:47 +0200

    Added documentation to FormattedTextDataElement

--- WoWsShipBuilder.Core/DataUI/DataElements/FormattedTextDataElement.cs
+++ WoWsShipBuilder.Core/DataUI/DataElements/FormattedTextDataElement.cs
@@ -2,4 +2,11 @@
 
 namespace WoWsShipBuilder.Core.DataUI.DataElements;
 
+/// <summary>
+/// A record that represent a formatted text data element.
+/// </summary>
+/// <param name="Text">The text to format. Need to respect <see cref="string.Format(string,object[])"/> specifications.</param>
+/// <param name="Values">The enumerable that will be used by <see cref="string.Format(string,object[])"/>.</param>
+/// <param name="IsTextKey">If <see cref="Text"/> is a localization key and not actual text.</param>
+/// <param name="AreValuesKeys">If <see cref="Values"/> are localization keys and not actual values.</param>
 public sealed record FormattedTextDataElement(string Text, IEnumerable<string> Values, bool IsTextKey, bool AreValuesKeys) : IDataElement;
