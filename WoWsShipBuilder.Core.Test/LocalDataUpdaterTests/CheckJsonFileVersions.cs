f7f89a2909d81d6a02b63c49aa1dc4485597cadc
floribe2000 <git@floribe2000.de>
2022-06-03 23:22:22 +0200

    DataUpdater no longer downloads incompatible data
    Also modified DesktopAppDataService to use a different data directory when the app runs in dev/debug mode.

--- WoWsShipBuilder.Core.Test/LocalDataUpdaterTests/CheckJsonFileVersions.cs
+++ WoWsShipBuilder.Core.Test/LocalDataUpdaterTests/CheckJsonFileVersions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Moq;
@@ -77,6 +78,46 @@ namespace WoWsShipBuilder.Core.Test.LocalDataUpdaterTests
             result.AvailableFileUpdates.Should().BeEmpty();
             result.ShouldImagesUpdate.Should().BeFalse();
             result.CanImagesDeltaUpdate.Should().BeFalse();
+            result.ShouldLocalizationUpdate.Should().BeFalse();
+        }
+
+        [Test]
+        public async Task CheckJsonFileVersions_OneVersionDiffNewVersionNotSupported()
+        {
+            // Arrange
+            var supportedDataVersion = Assembly.GetAssembly(typeof(Ship))!.GetName().Version!;
+            var currentVersion = new GameVersion(new(0, 10, 10), GameVersionType.Live, 1);
+            var previousVersion = new GameVersion(new(0, 10, 9), GameVersionType.Live, 1);
+            var previousVersionCode = 1;
+            var localVersionInfo = new VersionInfo(
+                new()
+                {
+                    { "Ability", new() { new("Common.json", previousVersionCode) } },
+                    { "Ship", new() { new("Japan.json", previousVersionCode), new("Germany.json", previousVersionCode) } },
+                },
+                previousVersionCode,
+                previousVersion);
+            var testVersionInfo = new VersionInfo(
+                new()
+                {
+                    { "Ability", new() { new("Common.json", 2) } },
+                    { "Ship", new() { new("Japan.json", 1), new("Germany.json", 1) } },
+                },
+                2,
+                currentVersion,
+                localVersionInfo.CurrentVersion)
+            {
+                DataStructuresVersion = new(supportedDataVersion.Major, supportedDataVersion.Minor + 1, supportedDataVersion.Build),
+            };
+            awsClientMock.Setup(x => x.DownloadVersionInfo(ServerType.Live)).ReturnsAsync(testVersionInfo);
+            mockFileSystem.AddDirectory(appDataHelper.Object.GetDataPath(ServerType.Live));
+            appDataHelper.Setup(x => x.GetLocalVersionInfo(ServerType.Live)).ReturnsAsync(localVersionInfo);
+
+            // Act
+            var result = await new LocalDataUpdater(mockFileSystem, awsClientMock.Object, appDataHelper.Object, new()).CheckJsonFileVersions(ServerType.Live);
+
+            // Assert
+            result.AvailableFileUpdates.Should().BeEmpty();
             result.ShouldLocalizationUpdate.Should().BeFalse();
         }
     }
