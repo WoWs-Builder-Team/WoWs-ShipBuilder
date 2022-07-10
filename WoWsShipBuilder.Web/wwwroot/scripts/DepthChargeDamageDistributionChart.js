adba07b2b3662db6373449d9dedd72a5608f4c38
yesod30 <samu.9301295@live.it>
2022-07-10 23:40:35 +0200

    initial work on depth charges damage distribution chart

---
+++ WoWsShipBuilder.Web/wwwroot/scripts/DepthChargeDamageDistributionChart.js
@@ -0,0 +1,11 @@
+﻿export function DrawDepthChargeDamageDistributionChart(id, dataRecord){
+    console.log(dataRecord);
+    const canvas = document.getElementById(id);
+    const ctx = canvas.getContext("2d");
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    canvas.width = width;
+    canvas.height = height;
+    console.log(width);
+    console.log(height);
+}
