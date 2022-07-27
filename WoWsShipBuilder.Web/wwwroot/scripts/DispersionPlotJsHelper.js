e60d7583f95e03ea7e08cdf253428d3da57a2e00
iTTou <perotti.denis@gmail.com>
2022-07-27 03:42:56 +0200

    Set min size for canvas. Minor code refactoring.

--- WoWsShipBuilder.Web/wwwroot/scripts/DispersionPlotJsHelper.js
+++ WoWsShipBuilder.Web/wwwroot/scripts/DispersionPlotJsHelper.js
@@ -17,53 +17,68 @@
                 canvas.height = Object.values(fusoReference)[1] * 2 + ellipseOffset;
             }
         }
-        let width = canvas.width;
-        let height = canvas.height;
-        let centerX = width / 2;
-        let centerY = height / 2;
-        const top = ellipseOffset / 2;
-        const bottom = height - ellipseOffset / 2;
-        const right = width - ellipseOffset / 2;
-        const left = ellipseOffset / 2;
+        if(canvas.width < 156){
+            canvas.width = 156;
+        }
+        if(canvas.height < 156){
+            canvas.height = 156;
+        }
+        const width = canvas.width;
+        const height = canvas.height;
+        const centerX = width / 2;
+        const centerY = height / 2;
+        const canvasTop = ellipseOffset / 2;
+        const canvasBottom = height - ellipseOffset / 2;
+        const canvasRight = width - ellipseOffset / 2;
+        const canvasLeft = ellipseOffset / 2;
+        const outerEllipseTop = centerY - drawingData.yRadiusOuterEllipse;
+        const outerEllipseBottom = centerY + drawingData.yRadiusOuterEllipse;
+        const outerEllipseRight = centerX + drawingData.xRadiusOuterEllipse;
+        const outerEllipseLeft = centerX - drawingData.xRadiusOuterEllipse;
+        const innerEllipseTop = centerY - drawingData.yRadiusInnerEllipse;
+        const innerEllipseBottom = centerY + drawingData.yRadiusInnerEllipse;
+        const innerEllipseRight = centerX + drawingData.xRadiusInnerEllipse;
+        const innerEllipseLeft = centerX - drawingData.xRadiusInnerEllipse;
 
         ctx.clearRect(0, 0, width, height);
-        ctx.lineWidth  = 1;
-        ctx.strokeStyle="gray";
+        
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = "gray";
         ctx.beginPath();
-        ctx.moveTo(centerX, top);
-        ctx.lineTo(centerX, bottom);
-        ctx.moveTo(left, centerY);
-        ctx.lineTo(right, centerY);
+        ctx.moveTo(centerX, outerEllipseTop);
+        ctx.lineTo(centerX, outerEllipseBottom);
+        ctx.moveTo(outerEllipseLeft, centerY);
+        ctx.lineTo(outerEllipseRight, centerY);
         ctx.stroke();
 
         if (Object.values(fusoReference)[0] !== 0 && Object.values(fusoReference)[1] !== 0){
-            ctx.lineWidth  = 4;
-            ctx.strokeStyle="rgba(0, 0, 0, 0.5)";
+            ctx.lineWidth = 4;
+            ctx.strokeStyle = "rgba(0, 0, 0, 0.5)";
             ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
             ctx.beginPath();
             ctx.ellipse(centerX, centerY, Object.values(fusoReference)[0], Object.values(fusoReference)[1], 0, 0, 2 * Math.PI);
             ctx.fill();
-            ctx.stroke()
+            ctx.stroke();
         }
         
-        ctx.lineWidth  = 4;
-        ctx.strokeStyle="gray";
+        ctx.lineWidth = 4;
+        ctx.strokeStyle = "gray";
         ctx.fillStyle = "rgba(128, 128, 128, 0.1)";
         ctx.beginPath();
         ctx.ellipse(centerX, centerY, drawingData.xRadiusOuterEllipse, drawingData.yRadiusOuterEllipse, 0, 0, 2 * Math.PI);
         ctx.fill();
-        ctx.stroke()
+        ctx.stroke();
 
+        ctx.lineWidth = 4;
         ctx.strokeStyle = "rgba(255 , 46 , 46, 0.5)";
-        ctx.lineWidth  = 4;
         ctx.fillStyle = "rgba(255 , 46 , 46, 0.1)";
         ctx.beginPath();
         ctx.ellipse(centerX, centerY, drawingData.xRadiusInnerEllipse, drawingData.yRadiusInnerEllipse, 0, 0, 2 * Math.PI);
         ctx.fill();
-        ctx.stroke()
+        ctx.stroke();
 
+        ctx.lineWidth = 1;
         ctx.strokeStyle = "transparent";
-        ctx.lineWidth  = 1;
         ctx.fillStyle = "rgba(218,165,32, 0.33)";
         let pointRadius = 6;
         if(scaling < 1){
@@ -73,52 +88,52 @@
             ctx.beginPath();
             ctx.ellipse(centerX - Object.values(point)[0], centerY - Object.values(point)[1], pointRadius, pointRadius, 0, 0, 2 * Math.PI);
             ctx.fill();
-            ctx.stroke()
+            ctx.stroke();
         });
         
-        ctx.lineWidth  = 1;
-        ctx.strokeStyle="gray";
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = "gray";
         ctx.beginPath();
-        ctx.moveTo(centerX - drawingData.xRadiusOuterEllipse, bottom + rulersOffset);
-        ctx.lineTo(centerX + drawingData.xRadiusOuterEllipse, bottom + rulersOffset);
-        ctx.moveTo(centerX - drawingData.xRadiusOuterEllipse, bottom + rulersOffset  + 10);
-        ctx.lineTo(centerX - drawingData.xRadiusOuterEllipse, bottom + rulersOffset - 10);
-        ctx.moveTo(centerX + drawingData.xRadiusOuterEllipse, bottom + rulersOffset + 10);
-        ctx.lineTo(centerX + drawingData.xRadiusOuterEllipse, bottom + rulersOffset - 10);
+        ctx.moveTo(outerEllipseLeft, canvasBottom + rulersOffset);
+        ctx.lineTo(outerEllipseRight, canvasBottom + rulersOffset);
+        ctx.moveTo(outerEllipseLeft, canvasBottom + rulersOffset + 10);
+        ctx.lineTo(outerEllipseLeft, canvasBottom + rulersOffset - 10);
+        ctx.moveTo(outerEllipseRight, canvasBottom + rulersOffset + 10);
+        ctx.lineTo(outerEllipseRight, canvasBottom + rulersOffset - 10);
 
-        ctx.moveTo(left - rulersOffset, centerY - drawingData.yRadiusOuterEllipse);
-        ctx.lineTo(left - rulersOffset, centerY + drawingData.yRadiusOuterEllipse);
-        ctx.moveTo(left - rulersOffset + 10, centerY - drawingData.yRadiusOuterEllipse);
-        ctx.lineTo(left - rulersOffset - 10, centerY - drawingData.yRadiusOuterEllipse);
-        ctx.moveTo(left - rulersOffset + 10, centerY + drawingData.yRadiusOuterEllipse);
-        ctx.lineTo(left - rulersOffset - 10, centerY + drawingData.yRadiusOuterEllipse);
+        ctx.moveTo(canvasLeft - rulersOffset, outerEllipseTop);
+        ctx.lineTo(canvasLeft - rulersOffset, outerEllipseBottom);
+        ctx.moveTo(canvasLeft - rulersOffset + 10, outerEllipseTop);
+        ctx.lineTo(canvasLeft - rulersOffset - 10, outerEllipseTop);
+        ctx.moveTo(canvasLeft - rulersOffset + 10, outerEllipseBottom);
+        ctx.lineTo(canvasLeft - rulersOffset - 10, outerEllipseBottom);
         ctx.stroke();
 
-        ctx.strokeStyle="rgb(255 , 46 , 46)";
+        ctx.strokeStyle = "rgb(255 , 46 , 46)";
         ctx.beginPath();
-        ctx.moveTo(centerX - drawingData.xRadiusInnerEllipse, top - rulersOffset);
-        ctx.lineTo(centerX + drawingData.xRadiusInnerEllipse, top - rulersOffset);
-        ctx.moveTo(centerX - drawingData.xRadiusInnerEllipse, top - rulersOffset + 10);
-        ctx.lineTo(centerX - drawingData.xRadiusInnerEllipse, top - rulersOffset - 10);
-        ctx.moveTo(centerX + drawingData.xRadiusInnerEllipse, top - rulersOffset + 10);
-        ctx.lineTo(centerX + drawingData.xRadiusInnerEllipse, top - rulersOffset - 10);
+        ctx.moveTo(innerEllipseLeft, canvasTop - rulersOffset);
+        ctx.lineTo(innerEllipseRight, canvasTop - rulersOffset);
+        ctx.moveTo(innerEllipseLeft, canvasTop - rulersOffset + 10);
+        ctx.lineTo(innerEllipseLeft, canvasTop - rulersOffset - 10);
+        ctx.moveTo(innerEllipseRight, canvasTop - rulersOffset + 10);
+        ctx.lineTo(innerEllipseRight, canvasTop - rulersOffset - 10);
 
-        ctx.moveTo(right + rulersOffset, centerY - drawingData.yRadiusInnerEllipse);
-        ctx.lineTo(right + rulersOffset, centerY + drawingData.yRadiusInnerEllipse);
-        ctx.moveTo(right + rulersOffset + 10, centerY - drawingData.yRadiusInnerEllipse);
-        ctx.lineTo(right + rulersOffset - 10, centerY - drawingData.yRadiusInnerEllipse);
-        ctx.moveTo(right + rulersOffset + 10, centerY + drawingData.yRadiusInnerEllipse);
-        ctx.lineTo(right + rulersOffset - 10, centerY + drawingData.yRadiusInnerEllipse);
+        ctx.moveTo(canvasRight + rulersOffset, innerEllipseTop);
+        ctx.lineTo(canvasRight + rulersOffset, innerEllipseBottom);
+        ctx.moveTo(canvasRight + rulersOffset + 10, innerEllipseTop);
+        ctx.lineTo(canvasRight + rulersOffset - 10, innerEllipseTop);
+        ctx.moveTo(canvasRight + rulersOffset + 10, innerEllipseBottom);
+        ctx.lineTo(canvasRight + rulersOffset - 10, innerEllipseBottom);
         ctx.stroke();
         
         ctx.font = "13px Roboto";
         ctx.fillStyle = "grey";
         ctx.textBaseline = "top";
         ctx.textAlign = "center";
-        ctx.fillText(`Max Vertical: ${Math.round(drawingData.xRadiusOuterEllipse * 2 / scaling)} m`, centerX, bottom + rulersOffset + 10);
+        ctx.fillText(`Max Vertical: ${Math.round(drawingData.xRadiusOuterEllipse * 2 / scaling)} m`, centerX, canvasBottom + rulersOffset + 10);
 
         ctx.save();
-        ctx.translate(left - rulersOffset - 10, centerY);
+        ctx.translate(canvasLeft - rulersOffset - 10, centerY);
         ctx.rotate(-Math.PI / 2);
         ctx.textBaseline = "bottom";
         ctx.fillText(`Max Horizontal: ${Math.round(drawingData.yRadiusOuterEllipse * 2 / scaling)} m`, 0, 0);
@@ -126,9 +141,9 @@
         
         ctx.fillStyle = "rgb(255 , 46 , 46)";
         ctx.textBaseline = "bottom";
-        ctx.fillText(`50% Hits Vertical: ${Math.round(drawingData.xRadiusInnerEllipse * 2 / scaling)} m`, centerX, top - rulersOffset - 10);
+        ctx.fillText(`50% Hits Vertical: ${Math.round(drawingData.xRadiusInnerEllipse * 2 / scaling)} m`, centerX, canvasTop - rulersOffset - 10);
 
-        ctx.translate(right + rulersOffset + 10, centerY);
+        ctx.translate(canvasRight + rulersOffset + 10, centerY);
         ctx.rotate(Math.PI / 2);
         ctx.textBaseline = "bottom";
         ctx.fillText(`50% Hits Horizontal: ${Math.round(drawingData.yRadiusInnerEllipse * 2 / scaling)} m`, 0, 0);
