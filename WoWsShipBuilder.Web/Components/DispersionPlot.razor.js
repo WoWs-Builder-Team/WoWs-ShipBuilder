25827e404e66ee0109978adade432086670390e5
iTTou <perotti.denis@gmail.com>
2022-08-03 22:20:15 +0200

    fix blurred text in dispersion plot

--- WoWsShipBuilder.Web/Components/DispersionPlot.razor.js
+++ WoWsShipBuilder.Web/Components/DispersionPlot.razor.js
@@ -1,12 +1,12 @@
 ﻿export function DrawDispersionPlotBatched(data, scaling, fusoReference, text){
+    const ratio = window.devicePixelRatio;
+    const ellipseOffset = 100;
+    const rulersOffset = 20;
     for(const key in data){
         const canvas = document.getElementById(key);
         const ctx = canvas.getContext("2d");
 
         const drawingData = data[key];
-
-        const ellipseOffset = 100;
-        const rulersOffset = 20;
 
         canvas.width = drawingData.xRadiusOuterEllipse * 2 + ellipseOffset;
         canvas.height = drawingData.yRadiusOuterEllipse * 2 + ellipseOffset;
@@ -25,21 +25,28 @@
         const bottomText = `${text.bottomLabel} ${Math.round(drawingData.xRadiusOuterEllipse * 2 / scaling)} ${text.meters}`;
         const leftText  = `${text.leftLabel} ${Math.round(drawingData.yRadiusOuterEllipse * 2 / scaling)} ${text.meters}`;
         const rightText = `${text.rightLabel} ${Math.round(drawingData.yRadiusInnerEllipse * 2 / scaling)} ${text.meters}`;
-        const minSize = ctx.measureText(
+        const textSize = ctx.measureText(
             [topText, bottomText, leftText, rightText].reduce(
                 function (a, b) {
                     return a.length > b.length ? a : b;
                 }
-            )).width;
+            ));
+        const minSize = textSize.width + 3 * (textSize.actualBoundingBoxAscent + textSize.actualBoundingBoxDescent);
         if(canvas.width < minSize){
             canvas.width = minSize;
         }
         if(canvas.height < minSize){
             canvas.height = minSize;
         }
-
-        const width = canvas.width;
-        const height = canvas.height;
+        
+        canvas.style.width = canvas.width + "px";
+        canvas.style.height = canvas.height + "px";
+        
+        canvas.width *= ratio;
+        canvas.height *= ratio;
+        
+        const width = canvas.width / ratio;
+        const height = canvas.height / ratio;
         const centerX = width / 2;
         const centerY = height / 2;
         const canvasTop = ellipseOffset / 2;
@@ -57,6 +64,7 @@
 
         ctx.clearRect(0, 0, width, height);
 
+        ctx.scale(ratio, ratio);
         ctx.lineWidth = 1;
         ctx.strokeStyle = "gray";
         ctx.beginPath();
@@ -140,18 +148,20 @@
         ctx.moveTo(canvasRight + rulersOffset + 10, innerEllipseBottom);
         ctx.lineTo(canvasRight + rulersOffset - 10, innerEllipseBottom);
         ctx.stroke();
-
+        
+        ctx.font = "14px Roboto";
         ctx.fillStyle = "grey";
         ctx.textBaseline = "top";
         ctx.textAlign = "center";
         ctx.fillText(bottomText, centerX, canvasBottom + rulersOffset + 10);
-
+        
         ctx.textBaseline = "bottom";
         ctx.translate(canvasLeft - rulersOffset - 10, centerY);
         ctx.rotate(-Math.PI / 2);
         ctx.fillText(leftText, 0, 0);
         ctx.resetTransform();
 
+        ctx.scale(ratio, ratio);
         ctx.fillStyle = "rgb(255 , 46 , 46)";
         ctx.fillText(topText, centerX, canvasTop - rulersOffset - 10);
         ctx.translate(canvasRight + rulersOffset + 10, centerY);
