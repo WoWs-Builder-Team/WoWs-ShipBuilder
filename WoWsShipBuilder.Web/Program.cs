9443063b44484bb3f48cbbd7929b4c1587f11719
floribe2000 <git@floribe2000.de>
2022-08-10 22:22:40 +0200

    Prepare firebase api integration

--- WoWsShipBuilder.Web/Program.cs
+++ WoWsShipBuilder.Web/Program.cs
@@ -13,10 +13,16 @@ using WoWsShipBuilder.Web.Extensions;
 using WoWsShipBuilder.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true);
+});
 
-// Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddServerSideBlazor(options =>
+{
+    options.DisconnectedCircuitRetentionPeriod = TimeSpan.FromMinutes(10);
+});
 builder.ConfigureShipBuilderOptions();
 
 builder.Logging.ClearProviders();
@@ -40,9 +46,6 @@ app.Services.UseMicrosoftDependencyResolver();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
