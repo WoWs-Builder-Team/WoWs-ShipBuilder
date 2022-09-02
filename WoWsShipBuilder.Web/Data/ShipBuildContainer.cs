5b684503dd3d34dee280dc3b008c5c9aa0e2d368
floribe2000 <git@floribe2000.de>
2022-09-03 01:03:11 +0200

    Add build configuration dialog with test page and remove duplicate code

---
+++ WoWsShipBuilder.Web/Data/ShipBuildContainer.cs
@@ -0,0 +1,12 @@
+﻿using WoWsShipBuilder.Core.BuildCreator;
+using WoWsShipBuilder.DataStructures;
+
+namespace WoWsShipBuilder.Web.Data;
+
+public sealed record ShipBuildContainer(Ship Ship, Build? Build, Guid Id, IEnumerable<int>? ActivatedConsumableSlots)
+{
+    public static ShipBuildContainer CreateNew(Ship ship, Build? build, IEnumerable<int>? activatedConsumableSlots)
+    {
+        return new(ship, build, Guid.NewGuid(), activatedConsumableSlots);
+    }
+}
