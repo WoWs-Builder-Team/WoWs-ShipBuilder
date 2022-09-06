b1eed3070fa4f1a822061f6622b836d8a816184e
yesod30 <samu.9301295@live.it>
2022-09-06 17:39:29 +0200

    Fixed missing dots in documentation

--- WoWsShipBuilder.Core/DataContainers/AccelerationHelper.cs
+++ WoWsShipBuilder.Core/DataContainers/AccelerationHelper.cs
@@ -395,12 +395,12 @@ public static class AccelerationHelper
     /// <summary>
     /// Record to hold the data calculated by <see cref="AccelerationHelper"/>.
     /// </summary>
-    /// <param name="Speed">The speed at the time of the Time parameter</param>
+    /// <param name="Speed">The speed at the time of the Time parameter.</param>
     /// <param name="Time">The current time.</param>
     public sealed record AccelerationPoints(double Speed, double Time);
 
     /// <summary>
-    /// Wrapper holder for the data calculated by <see cref="AccelerationHelper"/>
+    /// Wrapper holder for the data calculated by <see cref="AccelerationHelper"/>.
     /// </summary>
     /// <param name="TimeForGear">List of times needed to reach the gear given in input to <see cref="AccelerationHelper.CalculateAcceleration"/>.</param>
     /// <param name="AccelerationPointsList">List of the points calculated.</param>
