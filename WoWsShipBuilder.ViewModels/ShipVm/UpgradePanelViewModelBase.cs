b9c4a4c35e5c78e8c93cf26b8e25a4b36e23d7ab
yesod30 <samu.9301295@live.it>
2022-09-15 12:10:21 +0200

    Added bypass for Nation.Common

--- WoWsShipBuilder.ViewModels/ShipVm/UpgradePanelViewModelBase.cs
+++ WoWsShipBuilder.ViewModels/ShipVm/UpgradePanelViewModelBase.cs
@@ -21,7 +21,7 @@ namespace WoWsShipBuilder.ViewModels.ShipVm
             List<Modernization> filteredModernizations = upgradeData.Select(entry => entry.Value)
                 .Where(m => !(m.BlacklistedShips?.Contains(ship.Name) ?? false))
                 .Where(m => m.ShipLevel?.Contains(ship.Tier) ?? false)
-                .Where(m => m.AllowedNations?.Contains(ship.ShipNation) ?? false)
+                .Where(m => ship.ShipNation == Nation.Common || (m.AllowedNations?.Contains(ship.ShipNation) ?? false))
                 .Where(m => m.ShipClasses?.Contains(ship.ShipClass) ?? false)
                 .Union(upgradeData.Select(entry => entry.Value).Where(m => m.AdditionalShips?.Contains(ship.Name) ?? false))
                 .ToList();
