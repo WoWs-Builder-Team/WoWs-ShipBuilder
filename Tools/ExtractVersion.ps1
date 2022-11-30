da3f93654b0caa704aa4a39680630805aa11865b
floribe2000 <git@floribe2000.de>
2022-12-01 00:14:02 +0100

    Add docker image build for web version

---
+++ Tools/ExtractVersion.ps1
@@ -0,0 +1,15 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [string]
+    $projectFile
+)
+
+$xml = [Xml] (Get-Content $projectFile)
+$versionPrefix = [Version] $xml.Project.PropertyGroup.VersionPrefix
+$versionSuffix = $xml.Project.PropertyGroup.VersionSuffix
+
+if ($versionSuffix) {
+    $versionSuffix = "-$versionSuffix";
+}
+
+return "$versionPrefix$versionSuffix"
