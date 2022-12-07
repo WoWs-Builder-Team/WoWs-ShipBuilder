e8afad50411e63cedcb5cad1ba22c41e5586b44c
floribe2000 <git@floribe2000.de>
2022-12-08 00:57:18 +0100

    Implement Nerdbank.GitVersioning for desktop and web project

--- WoWsShipBuilder.ReleaseTest/ReleaseTest.cs
+++ WoWsShipBuilder.ReleaseTest/ReleaseTest.cs
@@ -7,34 +7,36 @@ using FluentAssertions;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using WoWsShipBuilder.UI;
+using WoWsShipBuilder.ViewModels.Helper;
 
-namespace WoWsShipBuilder.ReleaseTest
+namespace WoWsShipBuilder.ReleaseTest;
+
+public class Tests
 {
-    public class Tests
+    [Test]
+    public void EnsureReleaseVersion()
     {
-        [Test]
-        public void EnsureReleaseVersion()
-        {
-            var assembly = Assembly.GetAssembly(typeof(DataHelper));
-            string versionString = assembly!.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
-            Version.TryParse(versionString, out _).Should().BeTrue();
-        }
+        var assembly = Assembly.GetAssembly(typeof(DataHelper));
+        string versionString = assembly!.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
+        versionString = VersionHelper.StripCommitFromVersion(versionString);
+        Version.TryParse(versionString, out _).Should().BeTrue();
+    }
 
-        [Test]
-        public async Task EnsureNewVersionIsIncrement()
-        {
-            var assembly = Assembly.GetAssembly(typeof(DataHelper));
-            string versionString = assembly!.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
-            var version = Version.Parse(versionString);
+    [Test]
+    public async Task EnsureNewVersionIsIncrement()
+    {
+        var assembly = Assembly.GetAssembly(typeof(DataHelper));
+        string versionString = assembly!.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
+        versionString = VersionHelper.StripCommitFromVersion(versionString);
+        var version = Version.Parse(versionString);
 
-            const string uri = "https://api.github.com/repos/WoWs-Builder-Team/WoWs-ShipBuilder/releases/latest";
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Add("User-Agent", "C# test client");
-            var response = await client.GetStringAsync(uri);
-            var apiResponse = JsonConvert.DeserializeObject<GithubApiResponse>(response);
-            var lastVersionString = Regex.Replace(apiResponse!.Name, "[^0-9.]", string.Empty);
-            var lastVersion = Version.Parse(lastVersionString);
-            (lastVersion < version).Should().BeTrue();
-        }
+        const string uri = "https://api.github.com/repos/WoWs-Builder-Team/WoWs-ShipBuilder/releases/latest";
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", "C# test client");
+        var response = await client.GetStringAsync(uri);
+        var apiResponse = JsonConvert.DeserializeObject<GithubApiResponse>(response);
+        var lastVersionString = Regex.Replace(apiResponse!.Name, "[^0-9.]", string.Empty);
+        var lastVersion = Version.Parse(lastVersionString);
+        (lastVersion < version).Should().BeTrue();
     }
 }
