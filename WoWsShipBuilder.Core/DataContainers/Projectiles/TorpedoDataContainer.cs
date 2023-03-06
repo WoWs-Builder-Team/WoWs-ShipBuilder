621142db88e0bdaffb7b488a7e1ddf7eef43ce14
iTTou <perotti.denis@gmail.com>
2023-03-06 17:41:20 +0100

    Add more details and cut-off distances info for magnetic torps

--- WoWsShipBuilder.Core/DataContainers/Projectiles/TorpedoDataContainer.cs
+++ WoWsShipBuilder.Core/DataContainers/Projectiles/TorpedoDataContainer.cs
@@ -46,6 +46,72 @@ public partial record TorpedoDataContainer : ProjectileDataContainer
     [DataElementFiltering(false)]
     public decimal SplashCoeff { get; set; }
 
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "MaxTurningSpeed", UnitKey = "MPS", NameLocalizationKey = "FirstPing")]
+    public decimal MaxTurningSpeedFirstPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "MaxTurningSpeed", UnitKey = "MPS", NameLocalizationKey = "SecondPing")]
+    public decimal MaxTurningSpeedSecondPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "TurningAcceleration", UnitKey = "MPS2", NameLocalizationKey = "FirstPing")]
+    public decimal TurningAccelerationFirstPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "TurningAcceleration", UnitKey = "MPS2", NameLocalizationKey = "SecondPing")]
+    public decimal TurningAccelerationSecondPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "MaxVerticalSpeed", UnitKey = "MPS", NameLocalizationKey = "FirstPing")]
+    public decimal MaxVerticalSpeedFirstPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "MaxVerticalSpeed", UnitKey = "MPS", NameLocalizationKey = "SecondPing")]
+    public decimal MaxVerticalSpeedSecondPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "VerticalAcceleration", UnitKey = "MPS2", NameLocalizationKey = "FirstPing")]
+    public decimal VerticalAccelerationFirstPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "VerticalAcceleration", UnitKey = "MPS2", NameLocalizationKey = "SecondPing")]
+    public decimal VerticalAccelerationSecondPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "SearchRadius", UnitKey = "KM", NameLocalizationKey = "FirstPing")]
+    public decimal SearchRadiusFirstPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "SearchRadius", UnitKey = "KM", NameLocalizationKey = "SecondPing")]
+    public decimal SearchRadiusSecondPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "SearchAngle", UnitKey = "Degree", NameLocalizationKey = "FirstPing")]
+    public decimal SearchAngleFirstPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "SearchAngle", UnitKey = "Degree", NameLocalizationKey = "SecondPing")]
+    public decimal SearchAngleSecondPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "Destroyer", UnitKey = "M", NameLocalizationKey = "FirstPing")]
+    public decimal DestroyerCutOffFirstPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "Destroyer", UnitKey = "M", NameLocalizationKey = "SecondPing")]
+    public decimal DestroyerCutOffSecondPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "Battleship", UnitKey = "M", NameLocalizationKey = "FirstPing")]
+    public decimal BattleshipCutOffFirstPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "Battleship", UnitKey = "M", NameLocalizationKey = "SecondPing")]
+    public decimal BattleshipCutOffSecondPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "Cruiser", UnitKey = "M", NameLocalizationKey = "FirstPing")]
+    public decimal CruiserCutOffFirstPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "Cruiser", UnitKey = "M", NameLocalizationKey = "SecondPing")]
+    public decimal CruiserCutOffSecondPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "Submarine", UnitKey = "M", NameLocalizationKey = "FirstPing")]
+    public decimal SubCutOffFirstPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "Submarine", UnitKey = "M", NameLocalizationKey = "SecondPing")]
+    public decimal SubCutOffSecondPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "AirCarrier", UnitKey = "M", NameLocalizationKey = "FirstPing")]
+    public decimal CvCutOffFirstPing { get; set; }
+
+    [DataElementType(DataElementTypes.Grouped | DataElementTypes.KeyValueUnit, GroupKey = "AirCarrier", UnitKey = "M", NameLocalizationKey = "SecondPing")]
+    public decimal CvCutOffSecondPing { get; set; }
+
     public List<ShipClass>? CanHitClasses { get; set; }
 
     public bool IsLast { get; set; }
@@ -92,6 +158,39 @@ public partial record TorpedoDataContainer : ProjectileDataContainer
                 SplashCoeff = (decimal)torp.SplashCoeff,
                 IsFromPlane = fromPlane,
             };
+
+            if (torp.TorpedoType == DataStructures.TorpedoType.Magnetic)
+            {
+                torpedoDataContainer.MaxTurningSpeedFirstPing = Math.Round((decimal)torp.MagneticTorpedoParams.MaxTurningSpeed.First(), 1);
+                torpedoDataContainer.MaxTurningSpeedSecondPing = Math.Round((decimal)torp.MagneticTorpedoParams.MaxTurningSpeed.Last(), 1);
+                torpedoDataContainer.TurningAccelerationFirstPing = Math.Round((decimal)torp.MagneticTorpedoParams.TurningAcceleration.First(), 1);
+                torpedoDataContainer.TurningAccelerationSecondPing = Math.Round((decimal)torp.MagneticTorpedoParams.TurningAcceleration.Last(), 1);
+                torpedoDataContainer.MaxVerticalSpeedFirstPing = Math.Round((decimal)torp.MagneticTorpedoParams.MaxVerticalSpeed.First(), 1);
+                torpedoDataContainer.MaxVerticalSpeedSecondPing = Math.Round((decimal)torp.MagneticTorpedoParams.MaxVerticalSpeed.Last(), 1);
+                torpedoDataContainer.VerticalAccelerationFirstPing = Math.Round((decimal)torp.MagneticTorpedoParams.VerticalAcceleration.First(), 1);
+                torpedoDataContainer.VerticalAccelerationSecondPing = Math.Round((decimal)torp.MagneticTorpedoParams.VerticalAcceleration.Last(), 1);
+                torpedoDataContainer.SearchRadiusFirstPing = Math.Round((decimal)torp.MagneticTorpedoParams.SearchRadius.First(), 1);
+                torpedoDataContainer.SearchRadiusSecondPing = Math.Round((decimal)torp.MagneticTorpedoParams.SearchRadius.Last(), 1);
+                torpedoDataContainer.SearchAngleFirstPing = Math.Round((decimal)torp.MagneticTorpedoParams.SearchAngle.First(), 1);
+                torpedoDataContainer.SearchAngleSecondPing = Math.Round((decimal)torp.MagneticTorpedoParams.SearchAngle.Last(), 1);
+
+                var ddCutOff = torp.MagneticTorpedoParams.DropTargetAtDistance.First(x => x.Key == ShipClass.Destroyer).Value;
+                var bbCutOff = torp.MagneticTorpedoParams.DropTargetAtDistance.First(x => x.Key == ShipClass.Battleship).Value;
+                var caCutOff = torp.MagneticTorpedoParams.DropTargetAtDistance.First(x => x.Key == ShipClass.Cruiser).Value;
+                var subCutOff = torp.MagneticTorpedoParams.DropTargetAtDistance.First(x => x.Key == ShipClass.Submarine).Value;
+                var cvCutOff = torp.MagneticTorpedoParams.DropTargetAtDistance.First(x => x.Key == ShipClass.AirCarrier).Value;
+
+                torpedoDataContainer.DestroyerCutOffFirstPing = Math.Round((decimal)ddCutOff.First(), 1);
+                torpedoDataContainer.DestroyerCutOffSecondPing = Math.Round((decimal)ddCutOff.Last(), 1);
+                torpedoDataContainer.BattleshipCutOffFirstPing = Math.Round((decimal)bbCutOff.First(), 1);
+                torpedoDataContainer.BattleshipCutOffSecondPing = Math.Round((decimal)bbCutOff.Last(), 1);
+                torpedoDataContainer.CruiserCutOffFirstPing = Math.Round((decimal)caCutOff.First(), 1);
+                torpedoDataContainer.CruiserCutOffSecondPing = Math.Round((decimal)caCutOff.Last(), 1);
+                torpedoDataContainer.SubCutOffFirstPing = Math.Round((decimal)subCutOff.First(), 1);
+                torpedoDataContainer.SubCutOffSecondPing = Math.Round((decimal)subCutOff.Last(), 1);
+                torpedoDataContainer.CvCutOffFirstPing = Math.Round((decimal)cvCutOff.First(), 1);
+                torpedoDataContainer.CvCutOffSecondPing = Math.Round((decimal)cvCutOff.Last(), 1);
+            }
 
             if (torp.IgnoreClasses != null && torp.IgnoreClasses.Any())
             {
