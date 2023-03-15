d034e3f37155fc5cae6df9e8302475c00b8c08a3
iTTou <perotti.denis@gmail.com>
2023-03-15 20:34:57 +0100

    Initial work on build image rendering for web

---
+++ WoWsShipBuilder.Web/Dialogs/BuildImageRenderDialog.razor.js
@@ -0,0 +1,12 @@
+﻿export async function downloadBuildImage(id, buildName){
+    let img = "";
+    await html2canvas(document.querySelector("#" + id), {
+        backgroundColor: "#282828",
+        useCORS: true,
+        allowTaint: true,
+    }).then(canvas => img = canvas.toDataURL("image/png"));
+    let d = document.createElement("a");
+    d.href = img;
+    d.download = buildName + ".png";
+    d.click();
+}
