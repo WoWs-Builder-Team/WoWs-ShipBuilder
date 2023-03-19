26d0a4c64e8ae57a6f79804dfa52eb6f711e1f34
iTTou <perotti.denis@gmail.com>
2023-03-19 13:07:32 +0100

    Add edit build's name. Dialog now resizes based on the user image settings

--- WoWsShipBuilder.Web/Dialogs/BuildImageRenderDialog.razor.js
+++ WoWsShipBuilder.Web/Dialogs/BuildImageRenderDialog.razor.js
@@ -4,6 +4,10 @@
         backgroundColor: "#282828",
         useCORS: true,
         allowTaint: true,
+        ignoreElements: function( element ) {
+            if( 'editBuildNameIcon' === element.id ) {
+                return true;
+            }},
     }).then(canvas => img = canvas.toDataURL("image/png"));
     let d = document.createElement("a");
     d.href = img;
