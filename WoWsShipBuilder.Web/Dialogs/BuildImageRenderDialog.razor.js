def315182b6d997b06a865ef18d480f28168c37e
iTTou <perotti.denis@gmail.com>
2023-03-20 10:47:14 +0100

    First attempt at encoding the build string into the build image

--- WoWsShipBuilder.Web/Dialogs/BuildImageRenderDialog.razor.js
+++ WoWsShipBuilder.Web/Dialogs/BuildImageRenderDialog.razor.js
@@ -1,4 +1,4 @@
-﻿export async function downloadBuildImage(id, buildName){
+﻿export async function downloadBuildImage(id, buildName, buildString){
     let img = "";
     await html2canvas(document.querySelector("#" + id), {
         backgroundColor: "#282828",
@@ -8,7 +8,69 @@
             if( 'editBuildNameIcon' === element.id ) {
                 return true;
             }},
-    }).then(canvas => img = canvas.toDataURL("image/png"));
+    }).then( function( canvas ) {
+        /*const ctx = canvas.getContext("2d");
+        const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const data = pixels.data;
+        let state = 'encoding';
+        let charIndex = 0;
+        let charValue = 0;
+        let pixelElementIndex = 0;
+        let zeros = 0;
+        for (let i = 0; i < data.length; i += 4) {
+            let red = data[i] - (data[i] % 2);
+            let green = data[i + 1] - (data[i + 1] % 2);
+            let blue = data[i + 2] - (data[i + 2] % 2);
+            for(let n = 0; n < 3; n++) {
+                if (pixelElementIndex % 8 === 0) {
+                    if (state === 'finishing' && zeros === 8) {
+                        if ((pixelElementIndex - 1) % 3 < 2) {
+                            data[i] = red;
+                            data[i + 1] = green;
+                            data[i + 2] = blue;
+                        }
+                        ctx.putImageData(pixels, 0, 0);
+                        break;
+                    }
+                    if (charIndex >= buildString.length) {
+                        state = 'finishing';
+                    }
+                    else {
+                        charValue = buildString[charIndex++];
+                    }
+                }
+                switch (pixelElementIndex % 3) {
+                    case 0:
+                        if (state === 'encoding') {
+                            red += charValue % 2;
+                            charValue /= 2;
+                        }
+                        break;
+                    case 1:
+                        if (state === 'encoding') {
+                            green += charValue % 2;
+                            charValue /= 2;
+                        }
+                        break;
+                    case 2:
+                        if (state === 'encoding') {
+                            blue += charValue % 2;
+                            charValue /= 2;
+                        }
+                        data[i] = red;
+                        data[i + 1] = green;
+                        data[i + 2] = blue;
+                        break;
+                }
+                pixelElementIndex++;
+                if (state === 'finishing') {
+                    zeros++;
+                }
+            }
+        }
+        ctx.putImageData(pixels, 0, 0);*/
+        img = canvas.toDataURL("image/png", 1.0);
+        });
     let d = document.createElement("a");
     d.href = img;
     d.download = buildName + ".png";
