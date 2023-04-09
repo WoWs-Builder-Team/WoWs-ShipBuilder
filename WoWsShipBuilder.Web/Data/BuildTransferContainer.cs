575645ee9faabafd66c9fc27830bc21df4770709
floribe2000 <git@floribe2000.de>
2023-04-09 23:56:35 +0200

    improve build data transfer between pages
    uses a scoped service instead of the local storage to pass builds when changing pages which should improve performance

---
+++ WoWsShipBuilder.Web/Data/BuildTransferContainer.cs
@@ -0,0 +1,3 @@
+﻿namespace WoWsShipBuilder.Web.Data;
+
+public sealed record BuildTransferContainer(string ShipIndex, string BuildString, List<(string, float)>? Modifiers, IEnumerable<int>? ActivatedConsumables);
---
+++ WoWsShipBuilder.Web/Services/SessionStateCache.cs
@@ -0,0 +1,28 @@
+﻿using WoWsShipBuilder.Web.Data;
+
+namespace WoWsShipBuilder.Web.Services;
+
+/**
+ * A caching service that stores <see cref="BuildTransferContainer"/> instances when navigating between pages.
+ */
+public class SessionStateCache
+{
+    private List<BuildTransferContainer>? buildTransferContainers;
+
+    public List<BuildTransferContainer>? GetAndResetBuildTransferContainers()
+    {
+        var result = buildTransferContainers;
+        buildTransferContainers = null;
+        return result;
+    }
+
+    public void SetBuildTransferContainers(List<BuildTransferContainer> containers)
+    {
+        buildTransferContainers = containers;
+    }
+
+    public void SetBuildTransferContainers(BuildTransferContainer container)
+    {
+        buildTransferContainers = new() { container };
+    }
+}
