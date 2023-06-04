f6fe87f4bfe77b4399376f3f0fa84ce0b8265a18
floribe2000 <git@floribe2000.de>
2023-06-05 00:20:43 +0200

    fix release test

--- WoWsShipBuilder.ReleaseTest/ReleaseTest.cs
+++ WoWsShipBuilder.ReleaseTest/ReleaseTest.cs
@@ -6,9 +6,7 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Newtonsoft.Json;
 using NUnit.Framework;
-using WoWsShipBuilder.Desktop;
-using WoWsShipBuilder.Desktop.ViewModels.Helper;
-using WoWsShipBuilder.UI;
+using WoWsShipBuilder.Desktop.Features.BlazorWebView;
 
 namespace WoWsShipBuilder.ReleaseTest;
 
@@ -17,18 +15,18 @@ public class Tests
     [Test]
     public void EnsureReleaseVersion()
     {
-        var assembly = Assembly.GetAssembly(typeof(DesignDataHelper));
+        var assembly = Assembly.GetAssembly(typeof(BlazorWindow));
         string versionString = assembly!.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
-        versionString = VersionHelper.StripCommitFromVersion(versionString);
+        versionString = StripCommitFromVersion(versionString);
         Version.TryParse(versionString, out _).Should().BeTrue();
     }
 
     [Test]
     public async Task EnsureNewVersionIsIncrement()
     {
-        var assembly = Assembly.GetAssembly(typeof(DesignDataHelper));
+        var assembly = Assembly.GetAssembly(typeof(BlazorWindow));
         string versionString = assembly!.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
-        versionString = VersionHelper.StripCommitFromVersion(versionString);
+        versionString = StripCommitFromVersion(versionString);
         var version = Version.Parse(versionString);
 
         const string uri = "https://api.github.com/repos/WoWs-Builder-Team/WoWs-ShipBuilder/releases/latest";
@@ -39,5 +37,16 @@ public class Tests
         var lastVersionString = Regex.Replace(apiResponse!.Name, "[^0-9.]", string.Empty);
         var lastVersion = Version.Parse(lastVersionString);
         (lastVersion < version).Should().BeTrue();
+    }
+
+    private static string StripCommitFromVersion(string rawVersion)
+    {
+        int commitStartIndex = rawVersion.IndexOf("+", StringComparison.Ordinal);
+        if (commitStartIndex > 0)
+        {
+            rawVersion = rawVersion[..commitStartIndex];
+        }
+
+        return rawVersion;
     }
 }
