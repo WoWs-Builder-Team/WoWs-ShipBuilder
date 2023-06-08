49c4166686da5fdd6e276e9717400add6fe075bf
iTTo <13366390+iTTou@users.noreply.github.com>
2023-06-08 17:31:19 +0200

    Enable user saved builds on web (#234)
    * enable save and store for user's builds in web.
    import/export saved builds from web
    
    * add error notification when trying to laod builds from invalid files
    
    * Rebase onto dev
    
    * add saved builds to ShipAndShellSelectionDialog.
    adjust BuildConfigurationDialog size
    
    * Reduce internal spacing in various build components
    
    * Reduce space between skills and signal buttons
    
    * Change build link load button localization in BuildConfigurationDialog
    
    * apply requested changes
    
    * Add warning about local storage in web settings

---
+++ WoWsShipBuilder.Web/wwwroot/scripts/userSettings.js
@@ -0,0 +1,9 @@
+﻿export async function downloadObjectAsJson(exportObj, exportName) {
+    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    let downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+}
