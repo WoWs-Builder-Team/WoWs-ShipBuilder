d3f40896572e96ad6e311aa1dfa119699f672672
floribe2000 <git@floribe2000.de>
2023-06-08 18:07:12 +0200

    add shared counter for all page access counters and remove old, separate metrics
    also cleaned up some unnecessary elements of the metrics system
    
    #230

--- WoWsShipBuilder.Common/Infrastructure/Metrics/MetricsService.cs
+++ WoWsShipBuilder.Common/Infrastructure/Metrics/MetricsService.cs
@@ -2,30 +2,21 @@
 
 namespace WoWsShipBuilder.Infrastructure.Metrics;
 
-public class MetricsService : IMetricsService
+public class MetricsService
 {
-    public Counter ShipPageCount { get; } = Prometheus.Metrics.CreateCounter("ship_page_requests_total", "Number of ship page requests");
+    public Counter PageAccessCount { get; } = Prometheus.Metrics.CreateCounter("page_access_total", "Number of page accesses", new CounterConfiguration
+    {
+        LabelNames = new[] { "page" },
+    });
 
-    public Counter ShipCount { get; } = Prometheus.Metrics.CreateCounter("ship_requests_total", "Number of viewed ships.", new CounterConfiguration
+    public Counter ShipViewCount { get; } = Prometheus.Metrics.CreateCounter("ship_views_total", "Number of viewed ships.", new CounterConfiguration
     {
         LabelNames = new[] { "ship_index", "ship_name" },
     });
 
-    public Counter BallisticPageCount { get; } = Prometheus.Metrics.CreateCounter("ballistics_page_requests_total", "Number of ballistics page requests");
+    public Histogram ComparisonLoadDuration { get; } = Prometheus.Metrics.CreateHistogram("comparison_load_duration_seconds", "Load duration for the ship comparison data grid in seconds");
 
-    public Counter AccelerationPageCount { get; } = Prometheus.Metrics.CreateCounter("acceleration_page_requests_total", "Number of acceleration page requests");
-
-    public Counter ComparisonPageCount { get; } = Prometheus.Metrics.CreateCounter("comparison_page_requests_total", "Number of comparison page requests");
-
-    public Summary ComparisonLoadDuration { get; } = Prometheus.Metrics.CreateSummary("comparison_load_duration_average10s", "Average load duration for the ship comparison data grid", new SummaryConfiguration
-    {
-        MaxAge = TimeSpan.FromSeconds(10),
-    });
-
-    public Summary ShipViewModelInitDuration { get; } = Prometheus.Metrics.CreateSummary("ship_vm_init_time_average10s", "Average init-time of the ship VM in seconds over the last 10 seconds.", new SummaryConfiguration
-    {
-        MaxAge = TimeSpan.FromSeconds(10),
-    });
+    public Histogram ShipViewModelInitDuration { get; } = Prometheus.Metrics.CreateHistogram("ship_vm_init_duration_seconds", "Init-time of the ship VM in seconds");
 
     public Counter RefCount { get; } = Prometheus.Metrics.CreateCounter("ship_page_refs_total", "Number of refs for ship page requests", new CounterConfiguration
     {
