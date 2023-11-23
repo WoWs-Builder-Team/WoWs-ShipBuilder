a272e2a6fade4596aea67fdaf7ab2f30f7d4988c
iTTou <13366390+iTTou@users.noreply.github.com>
2023-11-23 22:55:18 +0100

    Fix mudblazor breaking dataGrid sorting by list

--- WoWsShipBuilder.Common/Features/ShipComparison/GridData/SecondaryGridDataWrapper.cs
+++ WoWsShipBuilder.Common/Features/ShipComparison/GridData/SecondaryGridDataWrapper.cs
@@ -8,14 +8,14 @@ public class SecondaryGridDataWrapper
     public SecondaryGridDataWrapper(IReadOnlyCollection<SecondaryBatteryDataContainer>? secondaryBattery)
     {
         // Secondaries
-        this.Caliber = secondaryBattery?.Select(x => x.GunCaliber).ToList() ?? new();
-        this.BarrelCount = secondaryBattery?.Select(x => x.BarrelsCount).ToList() ?? new();
-        this.BarrelsLayout = secondaryBattery?.Select(x => x.BarrelsLayout).ToList() ?? new();
+        this.Caliber = secondaryBattery?.Select(x => x.GunCaliber).ToNoSortList() ?? new();
+        this.BarrelCount = secondaryBattery?.Select(x => x.BarrelsCount).ToNoSortList() ?? new();
+        this.BarrelsLayout = secondaryBattery?.Select(x => x.BarrelsLayout).ToNoSortList() ?? new();
         this.Range = secondaryBattery?.Select(x => x.Range).First();
-        this.Reload = secondaryBattery?.Select(x => x.Reload).ToList() ?? new();
-        this.RoF = secondaryBattery?.Select(x => x.RoF).ToList() ?? new();
-        this.Dpm = secondaryBattery?.Select(x => x.TheoreticalDpm).ToList() ?? new();
-        this.Fpm = secondaryBattery?.Select(x => x.PotentialFpm).ToList() ?? new();
+        this.Reload = secondaryBattery?.Select(x => x.Reload).ToNoSortList() ?? new();
+        this.RoF = secondaryBattery?.Select(x => x.RoF).ToNoSortList() ?? new();
+        this.Dpm = secondaryBattery?.Select(x => x.TheoreticalDpm).ToNoSortList() ?? new();
+        this.Fpm = secondaryBattery?.Select(x => x.PotentialFpm).ToNoSortList() ?? new();
         this.Sigma = secondaryBattery?.Select(x => x.Sigma).First();
         this.DispersionData = secondaryBattery?.Select(x => x.DispersionData).ToList() ?? new();
         this.DispersionModifier = secondaryBattery?.Select(x => x.DispersionModifier).ToList() ?? new();
@@ -24,25 +24,25 @@ public class SecondaryGridDataWrapper
         var secondaryShellData = secondaryBattery?.Select(x => x.Shell).ToList();
 
         this.Type = secondaryShellData?.Select(x => x?.Type).First();
-        this.Mass = secondaryShellData?.Select(x => x?.Mass ?? 0).ToList() ?? new();
-        this.Damage = secondaryShellData?.Select(x => x?.Damage ?? 0).ToList() ?? new();
-        this.SplashRadius = secondaryShellData?.Select(x => x?.SplashRadius ?? 0).ToList() ?? new();
-        this.SplashDamage = secondaryShellData?.Select(x => x?.SplashDmg ?? 0).ToList() ?? new();
-        this.Penetration = secondaryShellData?.Select(x => x?.Penetration ?? 0).ToList() ?? new();
-        this.Speed = secondaryShellData?.Select(x => x?.ShellVelocity ?? 0).ToList() ?? new();
-        this.AirDrag = secondaryShellData?.Select(x => x?.AirDrag ?? 0).ToList() ?? new();
-        this.HeShellFireChance = secondaryShellData?.Select(x => x?.ShellFireChance ?? 0).ToList() ?? new();
-        this.HeBlastRadius = secondaryShellData?.Select(x => x?.ExplosionRadius ?? 0).ToList() ?? new();
-        this.HeBlastPenetration = secondaryShellData?.Select(x => x?.SplashCoeff ?? 0).ToList() ?? new();
-        this.SapOvermatch = secondaryShellData?.Select(x => x?.Overmatch ?? 0).ToList() ?? new();
-        this.SapRicochet = secondaryShellData?.Select(x => x?.RicochetAngles ?? default!).ToList() ?? new();
+        this.Mass = secondaryShellData?.Select(x => x?.Mass ?? 0).ToNoSortList() ?? new();
+        this.Damage = secondaryShellData?.Select(x => x?.Damage ?? 0).ToNoSortList() ?? new();
+        this.SplashRadius = secondaryShellData?.Select(x => x?.SplashRadius ?? 0).ToNoSortList() ?? new();
+        this.SplashDamage = secondaryShellData?.Select(x => x?.SplashDmg ?? 0).ToNoSortList() ?? new();
+        this.Penetration = secondaryShellData?.Select(x => x?.Penetration ?? 0).ToNoSortList() ?? new();
+        this.Speed = secondaryShellData?.Select(x => x?.ShellVelocity ?? 0).ToNoSortList() ?? new();
+        this.AirDrag = secondaryShellData?.Select(x => x?.AirDrag ?? 0).ToNoSortList() ?? new();
+        this.HeShellFireChance = secondaryShellData?.Select(x => x?.ShellFireChance ?? 0).ToNoSortList() ?? new();
+        this.HeBlastRadius = secondaryShellData?.Select(x => x?.ExplosionRadius ?? 0).ToNoSortList() ?? new();
+        this.HeBlastPenetration = secondaryShellData?.Select(x => x?.SplashCoeff ?? 0).ToNoSortList() ?? new();
+        this.SapOvermatch = secondaryShellData?.Select(x => x?.Overmatch ?? 0).ToNoSortList() ?? new();
+        this.SapRicochet = secondaryShellData?.Select(x => x?.RicochetAngles ?? default!).ToNoSortList() ?? new();
     }
 
-    public List<decimal> Caliber { get; }
+    public NoSortList<decimal> Caliber { get; }
 
-    public List<int> BarrelCount { get; }
+    public NoSortList<int> BarrelCount { get; }
 
-    public List<string> BarrelsLayout { get; }
+    public NoSortList<string> BarrelsLayout { get; }
 
     public decimal? Range { get; }
 
@@ -50,40 +50,40 @@ public class SecondaryGridDataWrapper
 
     public List<double> DispersionModifier { get; }
 
-    public List<decimal> Reload { get; }
+    public NoSortList<decimal> Reload { get; }
 
-    public List<decimal> RoF { get; }
+    public NoSortList<decimal> RoF { get; }
 
-    public List<string> Dpm { get; }
+    public NoSortList<string> Dpm { get; }
 
-    public List<decimal> Fpm { get; }
+    public NoSortList<decimal> Fpm { get; }
 
     public decimal? Sigma { get; }
 
     // Secondary shells
     public string? Type { get; }
 
-    public List<decimal> Mass { get; }
+    public NoSortList<decimal> Mass { get; }
 
-    public List<decimal> Damage { get; }
+    public NoSortList<decimal> Damage { get; }
 
-    public List<decimal> SplashRadius { get; }
+    public NoSortList<decimal> SplashRadius { get; }
 
-    public List<decimal> SplashDamage { get; }
+    public NoSortList<decimal> SplashDamage { get; }
 
-    public List<int> Penetration { get; }
+    public NoSortList<int> Penetration { get; }
 
-    public List<decimal> Speed { get; }
+    public NoSortList<decimal> Speed { get; }
 
-    public List<decimal> AirDrag { get; }
+    public NoSortList<decimal> AirDrag { get; }
 
-    public List<decimal> HeShellFireChance { get; }
+    public NoSortList<decimal> HeShellFireChance { get; }
 
-    public List<decimal> HeBlastRadius { get; }
+    public NoSortList<decimal> HeBlastRadius { get; }
 
-    public List<decimal> HeBlastPenetration { get; }
+    public NoSortList<decimal> HeBlastPenetration { get; }
 
-    public List<decimal> SapOvermatch { get; }
+    public NoSortList<decimal> SapOvermatch { get; }
 
-    public List<string> SapRicochet { get; }
+    public NoSortList<string> SapRicochet { get; }
 }
---
+++ WoWsShipBuilder.Common/Features/ShipComparison/NoSortList.cs
@@ -0,0 +1,18 @@
+﻿namespace WoWsShipBuilder.Features.ShipComparison;
+
+public class NoSortList<T> : List<T>, IComparable
+{
+    public NoSortList()
+    {
+    }
+
+    public NoSortList(IEnumerable<T> collection)
+        : base(collection)
+    {
+    }
+
+    public int CompareTo(object? obj)
+    {
+        return 0;
+    }
+}
