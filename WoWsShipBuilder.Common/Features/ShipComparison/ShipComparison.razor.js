9acd76766e326e0e063d1b718a4d0dc80724bb5f
iTTou <13366390+iTTou@users.noreply.github.com>
2023-12-22 20:47:44 +0100

    xlsx file exporter

---
+++ WoWsShipBuilder.Common/Features/ShipComparison/ShipComparison.razor.js
@@ -0,0 +1,10 @@
+﻿
+export function exportTableToExcel(section) {
+    let table = document.getElementsByClassName("mud-table-root")[0];
+    TableToExcel.convert(table, {
+        name: `wowssb-ship-comparison-grid.xlsx`,
+        sheet: {
+            name: section,
+        }
+    });
+}
