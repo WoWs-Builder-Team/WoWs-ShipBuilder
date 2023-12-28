77e74ee06ff80641457fc1bc8a064023fbb32c83
iTTou <13366390+iTTou@users.noreply.github.com>
2023-12-28 16:36:40 +0100

    integrate metrics logging into the service

--- WoWsShipBuilder.Common/Infrastructure/DataTransfer/AppPagesBrowser.cs
+++ WoWsShipBuilder.Common/Infrastructure/DataTransfer/AppPagesBrowser.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MudBlazor;
+using WoWsShipBuilder.Infrastructure.Metrics;
 
 namespace WoWsShipBuilder.Infrastructure.DataTransfer;
 
@@ -14,16 +15,26 @@ public class AppPagesBrowser
 
     private readonly SessionStateCache sessionStateCache;
     private readonly NavigationManager navManager;
+    private readonly MetricsService metricsService;
 
-    public AppPagesBrowser(SessionStateCache sessionStateCache, NavigationManager navManager)
+    public AppPagesBrowser(SessionStateCache sessionStateCache, NavigationManager navManager, MetricsService metricsService)
     {
         this.sessionStateCache = sessionStateCache;
         this.navManager = navManager;
+        this.metricsService = metricsService;
     }
 
-    private void GoToPage(string basePageUrl, List<ShipBuildContainer> selectionList, ISnackbar? snackbar = null, string? shellIndex = null)
+    public enum AppPage
     {
-        int selectionCount = selectionList.Count;
+        ShipStats,
+        BallisticCharts,
+        AccelerationCharts,
+        ShipComparison,
+    }
+
+    private void GoToPage(string destinationPageBaseUrl, List<ShipBuildContainer> containerList, string? shellIndex, ISnackbar? snackbar, AppPage? leavingPage, string metricLabel)
+    {
+        int selectionCount = containerList.Count;
         switch (selectionCount)
         {
             case > TransferLimit:
@@ -33,27 +44,137 @@ public class AppPagesBrowser
                 snackbar?.Add($"No ships available. Eligible ships: {selectionCount}.", Severity.Error);
                 return;
             default:
-                this.sessionStateCache.SetBuildTransferContainers(selectionList);
-                this.navManager.NavigateTo(basePageUrl + string.Join(',', selectionList.Select(x => x.Ship.Index)) + (shellIndex is not null ? $"&shellIndex={shellIndex}" : string.Empty));
+                this.LogMetrics(leavingPage, destinationPageBaseUrl, metricLabel);
+                this.sessionStateCache.SetBuildTransferContainers(containerList);
+                this.navManager.NavigateTo(destinationPageBaseUrl + string.Join(',', containerList.Select(x => x.Ship.Index)) + (shellIndex is not null ? $"&shellIndex={shellIndex}" : string.Empty));
                 break;
         }
 
         // cant use await JsRuntime.InvokeAsync<object>("open", NavManager.BaseUri + url, "_blank"); to open in a new tab because builds are not carried over.
     }
 
-    public void GoToShipStats(ShipBuildContainer container, ISnackbar? snackbar = null) => this.GoToShipStats(new List<ShipBuildContainer> { container }, snackbar);
+    private void LogMetrics(AppPage? leavingPage, string destinationPageBaseUrl, string metricLabel)
+    {
+        switch (leavingPage)
+        {
+            case AppPage.ShipStats when destinationPageBaseUrl.Equals(BallisticChartsBaseUrl, StringComparison.Ordinal):
+                this.metricsService.ShipStatsActions.WithLabels(metricLabel).Inc();
+                break;
+            case AppPage.ShipStats when destinationPageBaseUrl.Equals(AccelerationChartsBaseUrl, StringComparison.Ordinal):
+                this.metricsService.ShipStatsActions.WithLabels(metricLabel).Inc();
+                break;
+            case AppPage.ShipStats when destinationPageBaseUrl.Equals(ShipComparisonBaseUrl, StringComparison.Ordinal):
+                this.metricsService.ShipStatsActions.WithLabels(metricLabel).Inc();
+                break;
+            case AppPage.BallisticCharts when destinationPageBaseUrl.Equals(ShipStatsBaseUrl, StringComparison.Ordinal):
+                this.metricsService.BallisticChartsActions.WithLabels(metricLabel).Inc();
+                break;
+            case AppPage.BallisticCharts when destinationPageBaseUrl.Equals(AccelerationChartsBaseUrl, StringComparison.Ordinal):
+                this.metricsService.BallisticChartsActions.WithLabels(metricLabel).Inc();
+                break;
+            case AppPage.BallisticCharts when destinationPageBaseUrl.Equals(ShipComparisonBaseUrl, StringComparison.Ordinal):
+                this.metricsService.BallisticChartsActions.WithLabels(metricLabel).Inc();
+                break;
+            case AppPage.AccelerationCharts when destinationPageBaseUrl.Equals(BallisticChartsBaseUrl, StringComparison.Ordinal):
+                this.metricsService.AccelerationChartsActions.WithLabels(metricLabel).Inc();
+                break;
+            case AppPage.AccelerationCharts when destinationPageBaseUrl.Equals(ShipStatsBaseUrl, StringComparison.Ordinal):
+                this.metricsService.AccelerationChartsActions.WithLabels(metricLabel).Inc();
+                break;
+            case AppPage.AccelerationCharts when destinationPageBaseUrl.Equals(ShipComparisonBaseUrl, StringComparison.Ordinal):
+                this.metricsService.AccelerationChartsActions.WithLabels(metricLabel).Inc();
+                break;
+            case AppPage.ShipComparison when destinationPageBaseUrl.Equals(BallisticChartsBaseUrl, StringComparison.Ordinal):
+                this.metricsService.ShipComparisonActions.WithLabels(metricLabel).Inc();
+                break;
+            case AppPage.ShipComparison when destinationPageBaseUrl.Equals(AccelerationChartsBaseUrl, StringComparison.Ordinal):
+                this.metricsService.ShipComparisonActions.WithLabels(metricLabel).Inc();
+                break;
+            case AppPage.ShipComparison when destinationPageBaseUrl.Equals(ShipStatsBaseUrl, StringComparison.Ordinal):
+                this.metricsService.ShipComparisonActions.WithLabels(metricLabel).Inc();
+                break;
+            default:
+                return;
+        }
+    }
 
-    public void GoToShipStats(IEnumerable<ShipBuildContainer> selectionList, ISnackbar? snackbar = null) => this.GoToPage(ShipStatsBaseUrl, selectionList.ToList(), snackbar);
+    // to ship stats
+    public void GoToShipStats(IEnumerable<ShipBuildContainer> containerList, ISnackbar? userNotification, AppPage? leavingPage, string metricLabel) => this.GoToPage(ShipStatsBaseUrl, containerList.ToList(), null, userNotification, leavingPage, metricLabel);
 
-    public void GoToBallisticCharts(ShipBuildContainer container, ISnackbar? snackbar = null, string? shellIndex = null) => this.GoToPage(BallisticChartsBaseUrl, new() { container }, snackbar, shellIndex);
+    public void GoToShipStats(IEnumerable<ShipBuildContainer> containerList, AppPage? leavingPage, string metricLabel) => this.GoToShipStats(containerList, null, leavingPage, metricLabel);
 
-    public void GoToBallisticCharts(IEnumerable<ShipBuildContainer> selectionList, ISnackbar? snackbar = null) => this.GoToPage(BallisticChartsBaseUrl, selectionList.ToList(), snackbar);
+    public void GoToShipStats(IEnumerable<ShipBuildContainer> containerList, ISnackbar? userNotification) => this.GoToShipStats(containerList, userNotification, null, string.Empty);
 
-    public void GoToAccelerationCharts(ShipBuildContainer container, ISnackbar? snackbar = null) => this.GoToAccelerationCharts(new List<ShipBuildContainer> { container }, snackbar);
+    public void GoToShipStats(IEnumerable<ShipBuildContainer> containerList) => this.GoToShipStats(containerList, null);
 
-    public void GoToAccelerationCharts(IEnumerable<ShipBuildContainer> selectionList, ISnackbar? snackbar = null) => this.GoToPage(AccelerationChartsBaseUrl, selectionList.ToList(), snackbar);
+    public void GoToShipStats(ShipBuildContainer container, ISnackbar? userNotification, AppPage? leavingPage, string metricLabel) => this.GoToPage(ShipStatsBaseUrl, new() { container }, null, userNotification, leavingPage, metricLabel);
 
-    public void GoToShipComparison(ShipBuildContainer container, ISnackbar? snackbar = null) => this.GoToShipComparison(new List<ShipBuildContainer> { container }, snackbar);
+    public void GoToShipStats(ShipBuildContainer container, AppPage? leavingPage, string metricLabel) => this.GoToShipStats(container, null, leavingPage, metricLabel);
 
-    public void GoToShipComparison(IEnumerable<ShipBuildContainer> selectionList, ISnackbar? snackbar = null) => this.GoToPage(ShipComparisonBaseUrl, selectionList.ToList(), snackbar);
+    public void GoToShipStats(ShipBuildContainer container, ISnackbar? userNotification) => this.GoToShipStats(container, userNotification, null, string.Empty);
+
+    public void GoToShipStats(ShipBuildContainer container) => this.GoToShipStats(container, null, string.Empty);
+
+
+    // to ballistic charts
+    public void GoToBallisticCharts(ShipBuildContainer container, string? shellIndex, ISnackbar? userNotification, AppPage? leavingPage, string metricLabel) => this.GoToPage(BallisticChartsBaseUrl, new() { container }, shellIndex, userNotification, leavingPage, metricLabel);
+
+    public void GoToBallisticCharts(ShipBuildContainer container, ISnackbar? userNotification, AppPage? leavingPage, string metricLabel) => this.GoToBallisticCharts(container, null, userNotification, leavingPage, metricLabel);
+
+    public void GoToBallisticCharts(ShipBuildContainer container, string? shellIndex, ISnackbar? userNotification) => this.GoToBallisticCharts(container, shellIndex, userNotification, null, string.Empty);
+
+    public void GoToBallisticCharts(ShipBuildContainer container, ISnackbar? userNotification) => this.GoToBallisticCharts(container, null, userNotification);
+
+    public void GoToBallisticCharts(ShipBuildContainer container, string? shellIndex, AppPage? leavingPage, string metricLabel) => this.GoToBallisticCharts(container, shellIndex, null, leavingPage, metricLabel);
+
+    public void GoToBallisticCharts(ShipBuildContainer container, AppPage? leavingPage, string metricLabel) => this.GoToBallisticCharts(container, null, null, leavingPage, metricLabel);
+
+    public void GoToBallisticCharts(ShipBuildContainer container, string? shellIndex) => this.GoToBallisticCharts(container, shellIndex, null);
+
+    public void GoToBallisticCharts(ShipBuildContainer container) => this.GoToBallisticCharts(container, null, null, null, string.Empty);
+
+    public void GoToBallisticCharts(IEnumerable<ShipBuildContainer> selectionList, ISnackbar? userNotification, AppPage? leavingPage, string metricLabel) => this.GoToPage(BallisticChartsBaseUrl, selectionList.ToList(), null, userNotification, leavingPage, metricLabel);
+
+    public void GoToBallisticCharts(IEnumerable<ShipBuildContainer> selectionList, AppPage? leavingPage, string metricLabel) => this.GoToBallisticCharts(selectionList, null, leavingPage, metricLabel);
+
+    public void GoToBallisticCharts(IEnumerable<ShipBuildContainer> selectionList, ISnackbar? userNotification) => this.GoToBallisticCharts(selectionList, userNotification, null, string.Empty);
+
+    public void GoToBallisticCharts(IEnumerable<ShipBuildContainer> selectionList) => this.GoToBallisticCharts(selectionList, null);
+
+
+    // to acceleration charts
+    public void GoToAccelerationCharts(IEnumerable<ShipBuildContainer> containerList, ISnackbar? userNotification, AppPage? leavingPage, string metricLabel) => this.GoToPage(AccelerationChartsBaseUrl, containerList.ToList(), null, userNotification, leavingPage, metricLabel);
+
+    public void GoToAccelerationCharts(IEnumerable<ShipBuildContainer> containerList, AppPage? leavingPage, string metricLabel) => this.GoToAccelerationCharts(containerList, null, leavingPage, metricLabel);
+
+    public void GoToAccelerationCharts(IEnumerable<ShipBuildContainer> containerList, ISnackbar? userNotification) => this.GoToAccelerationCharts(containerList, userNotification, null, string.Empty);
+
+    public void GoToAccelerationCharts(IEnumerable<ShipBuildContainer> containerList) => this.GoToAccelerationCharts(containerList, null);
+
+    public void GoToAccelerationCharts(ShipBuildContainer container, ISnackbar? userNotification, AppPage? leavingPage, string metricLabel) => this.GoToPage(AccelerationChartsBaseUrl, new() { container }, null, userNotification, leavingPage, metricLabel);
+
+    public void GoToAccelerationCharts(ShipBuildContainer container, AppPage? leavingPage, string metricLabel) => this.GoToAccelerationCharts(container, null, leavingPage, metricLabel);
+
+    public void GoToAccelerationCharts(ShipBuildContainer container, ISnackbar? userNotification) => this.GoToAccelerationCharts(container, userNotification, null, string.Empty);
+
+    public void GoToAccelerationCharts(ShipBuildContainer container) => this.GoToAccelerationCharts(container, null);
+
+
+
+    // to ship comparison
+    public void GoToShipComparison(IEnumerable<ShipBuildContainer> selectionList, ISnackbar? userNotification, AppPage? leavingPage, string metricLabel) => this.GoToPage(ShipComparisonBaseUrl, selectionList.ToList(), null, userNotification, leavingPage, metricLabel);
+
+    public void GoToShipComparison(IEnumerable<ShipBuildContainer> selectionList, AppPage? leavingPage, string metricLabel) => this.GoToShipComparison(selectionList, null, leavingPage, metricLabel);
+
+    public void GoToShipComparison(IEnumerable<ShipBuildContainer> selectionList, ISnackbar? userNotification) => this.GoToShipComparison(selectionList, userNotification, null, string.Empty);
+
+    public void GoToShipComparison(IEnumerable<ShipBuildContainer> selectionList) => this.GoToShipComparison(selectionList, null);
+
+    public void GoToShipComparison(ShipBuildContainer container, ISnackbar? userNotification, AppPage? leavingPage, string metricLabel) => this.GoToPage(ShipComparisonBaseUrl, new() { container }, null, userNotification, leavingPage, metricLabel);
+
+    public void GoToShipComparison(ShipBuildContainer container, AppPage? leavingPage, string metricLabel) => this.GoToShipComparison(container, null, leavingPage, metricLabel);
+
+    public void GoToShipComparison(ShipBuildContainer container, ISnackbar? userNotification) => this.GoToShipComparison(container, userNotification, null, string.Empty);
+
+    public void GoToShipComparison(ShipBuildContainer container) => this.GoToShipComparison(container, null);
 }
