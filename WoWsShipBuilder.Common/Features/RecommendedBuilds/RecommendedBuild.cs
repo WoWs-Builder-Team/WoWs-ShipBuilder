f9da121f8a2eb680c35e1c0971400b865b39e68f
floribe2000 <git@floribe2000.de>
2025-02-18 23:43:00 +0100

    add compose file for dev database

--- WoWsShipBuilder.Common/Features/RecommendedBuilds/RecommendedBuild.cs
+++
@@ -1,5 +0,0 @@
-﻿using WoWsShipBuilder.Features.Navigation;
-
-namespace WoWsShipBuilder.Features.RecommendedBuilds;
-
-public record RecommendedBuild(ShipBuildContainer BuildContainer, string Notes);
