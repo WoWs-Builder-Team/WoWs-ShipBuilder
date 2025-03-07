64e1a3a7d8994fa8d6446a5b8fafad255e600355
iTTou <13366390+iTTou@users.noreply.github.com>
2025-03-07 22:07:21 +0100

    Divide new UI into separate components

---
+++ src/WoWsShipBuilder.Common/Features/RecommendedBuilds/RecommendedBuildViewModel.cs
@@ -0,0 +1,12 @@
+﻿using WoWsShipBuilder.Features.Builds;
+
+namespace WoWsShipBuilder.Features.RecommendedBuilds;
+
+public record RecommendedBuildViewModel(ShipBuildViewModel ViewModel)
+{
+    public string Notes { get; set; } = string.Empty;
+
+    public bool IsDeleted { get; set; }
+
+    public bool IsNew { get; set; }
+}
